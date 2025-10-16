a6001edc415325c6dfe7f696239a2c59185bebe7
meheraj786 <meherajhosen786@gmail.com>
2025-10-16 23:11:34 +0600

    bug fix

--- app.ts
+++ app.ts
@@ -20,10 +20,21 @@ app.use(compression());
 app.use(express.json());
 app.use(cookieParser());
 
-const CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || "http://localhost:3000";
+const allowedOrigins = [
+  "http://localhost:5173",
+  "http://localhost:3000",
+  process.env.CLIENT_ORIGIN,
+];
+
 app.use(
   cors({
-    origin: ["http://localhost:5173", CLIENT_ORIGIN],
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
   })
 );
