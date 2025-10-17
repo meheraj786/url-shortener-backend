dd3d05fba857199724f0330ee849a89972369846
meheraj786 <meherajhosen786@gmail.com>
2025-10-17 20:25:56 +0600

    bug fix

--- app.ts
+++ app.ts
@@ -28,16 +28,17 @@ const allowedOrigins = [
 
 app.use(
   cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:3000",
+      process.env.CLIENT_ORIGIN!,
+    ],
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
+
 
 app.use(router);
 app.get("/:shortId", redirectUrl);
